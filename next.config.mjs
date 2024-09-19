bb6fd2eb37f79157da12a626814fe95ed8946e3c
Prem Vispute <prem.v@gllfl.com>
2024-09-19 12:13:52 +0530

    config changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   optimizeFonts: false,
-  images: {
-    path: "/",
-  },
+  // images: {
+  //   path: "/",
+  // },
+  basePath: isProd ? "/Portfolio_New" : "",
+  assetPrefix: isProd ? "/Portfolio_New" : "",
 };
 
 export default nextConfig;
